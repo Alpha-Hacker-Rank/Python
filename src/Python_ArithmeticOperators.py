bfea9e0b438eec126d6289441bde57d03ad5b530
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-03-23 15:53:38 +0530

    Python-ArithmatcOperators

---
+++ src/Python_ArithmeticOperators.py
@@ -0,0 +1,11 @@
+# Mindula Dilthushan
+# Hacker Rank - Python
+# minduladilthushan1@gmail.com
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a+b)
+    print(a-b)
+    print(a*b)
