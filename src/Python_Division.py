ab3e16e45d2862f2f0a5b70fd835f8b6b4c6e305
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-03-23 15:58:52 +0530

    Python-Division

---
+++ src/Python_Division.py
@@ -0,0 +1,10 @@
+# Mindula Dilthushan
+# Hacker Rank - Python
+# minduladilthushan1@gmail.com
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a//b)
+    print(a/b)
