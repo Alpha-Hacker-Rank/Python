8aed9d22a8682acfd04e02028a185b1893ba8414
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-03-23 15:45:06 +0530

    Python-If-Else

---
+++ src/Python_If_Else.py
@@ -0,0 +1,17 @@
+# Mindula Dilthushan
+# Hacker Rank - Python
+# minduladilthushan1@gmail.com
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+n = int(input())
+if n % 2 == 1:
+    print("Weird")
+elif n % 2 == 0 and 2 <= n <= 5:
+    print("Not Weird")
+elif n % 2 == 0 and 6 <= n <= 20:
+    print("Weird")
+else:
+    print("Not Weird")
